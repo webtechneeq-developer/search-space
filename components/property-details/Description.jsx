8d20ec75c528926efea28b5d7b1951c8c044387e
webtechneeq <wt.kraheja2024@gmail.com>
2025-08-12 16:42:22 +0530

    Created Single Property Deatil Page7

--- components/property-details/Description.jsx
+++ components/property-details/Description.jsx
@@ -1,37 +1,23 @@
-"use client";
-import React, { useState } from "react";
+import React from "react";
 
 export default function Description() {
-  const [showMore, setShowMore] = useState(false);
-
   return (
     <>
+      {" "}
       <h5 className="fw-6 title">Description</h5>
       <p className="text-variant-1">
-        Located around an hour away from, between the Perche and the Iton
+        Located around an hour away from Paris, between the Perche and the Iton
         valley, in a beautiful wooded park bordered by a charming stream, this
         country property immediately seduces with its bucolic and soothing
         environment.
       </p>
-
-      {/* Wrap the hidden section in a sliding div */}
-      <div className={`slide-container ${showMore ? "open" : ""}`}>
-        <p className="mt-8 text-variant-1">
-          An ideal choice for sports and leisure enthusiasts who will be able to
-          take advantage of its swimming pool (11m x 5m), tennis court, gym and
-          sauna.
-        </p>
-      </div>
-
-      <a
-        href="#"
-        className="btn-view"
-        onClick={(e) => {
-          e.preventDefault();
-          setShowMore(!showMore);
-        }}
-      >
-        <span className="text">{showMore ? "View Less" : "View More"}</span>
+      <p className="mt-8 text-variant-1">
+        An ideal choice for sports and leisure enthusiasts who will be able to
+        take advantage of its swimming pool (11m x 5m), tennis court, gym and
+        sauna.
+      </p>
+      <a href="#" className="btn-view">
+        <span className="text">View More</span>
       </a>
     </>
   );
