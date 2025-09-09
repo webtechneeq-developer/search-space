2c058df301d90a42059eb5570dd15742e31f013c
webtechneeq-developer <developer@webtechneeq.com>
2025-09-09 13:03:08 +0530

    Add files via upload

---
+++ data/servicesWeOffer.js
@@ -0,0 +1,40 @@
+export const servicesWeOffer = [
+  {
+    imageSrc: "/images/Services_We_Offer/icon1.png",
+    title: "A coworking space is a shared office space",
+    description:
+      "where individuals, entrepreneurs, freelancers, and small teams can work alongside each other in a communal setting.",
+  },
+  {
+    imageSrc: "/images/Services_We_Offer/icon2.png",
+    title: "These spaces provide a flexible and affordable alternative",
+    description:
+      "to traditional office rentals, and are designed to foster collaboration, networking, and a sense of community among members.",
+  },
+  {
+    imageSrc: "/images/Services_We_Offer/icon3.png",
+    title:
+      "Coworking spaces are designed to provide a flexible and affordable alternative",
+    description:
+      "to traditional office spaces, and they often offer various amenities, such as high-speed internet, meeting rooms, printing and scanning facilities, and kitchen areas.",
+  },
+  {
+    imageSrc: "/images/Services_We_Offer/icon4.png",
+    title:
+      "Coworking spaces are community centers, collaboration hubs, and social spaces",
+    description:
+      "where workers from different backgrounds can come together to share expertise and explore new ideas. Members often exchange ideas, give feedback, and collaborate on projects even if they have unrelated work.",
+  },
+  {
+    imageSrc: "/images/Services_We_Offer/icon5.png",
+    title: "Coworking is about creating a better place to work",
+    description:
+      "and as a result, a better way to work. When you rent an office or desk at a coworking space, you gain coworkers to bounce ideas off of that can help you grow your business or career.",
+  },
+  {
+    imageSrc: "/images/Services_We_Offer/icon6.png",
+    title: "Our team at Search Spaces assist you in providing private cabins",
+    description:
+      "dedicated desks, meeting rooms, conference rooms at your preferred location with greater ease at pan India level with zero brokerage.",
+  },
+];
