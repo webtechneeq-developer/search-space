932efd399e532c3664f8b9b92362d72b78e85c41
webtechneeq-developer <developer@webtechneeq.com>
2025-09-11 17:45:28 +0530

    Add files via upload

---
+++ components/otherPages/coworking_pages/Coworking_Flexi_Desk.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Image from "next/image";
+import Locations from "@/components/common/Locations";
+import { properties } from "@/data/demoProporties";
+
+export default function Coworking_Flexi_Desk() {
+  return (
+    <>
+      <div className="mt-20 mb-10">
+        <Locations properties={properties} />
+      </div>
+    </>
+  );
+}
---
+++ components/otherPages/coworking_pages/Coworking_Meeting_Room.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Locations from "@/components/common/Locations";
+import { properties } from "@/data/demoProporties";
+export default function Coworking_Meeting_Room() {
+  return (
+    <>
+      <div className="mt-20 mb-10">
+        <Locations properties={properties} />
+      </div>
+    </>
+  );
+}
