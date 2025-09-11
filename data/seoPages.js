8e79d8759cd02af6b7d0130ee033b67e10c8ba84
webtechneeq-developer <developer@webtechneeq.com>
2025-09-11 17:53:46 +0530

    Update seoPages.js

--- data/seoPages.js
+++ data/seoPages.js
@@ -1,6 +1,6 @@
 // seoPagesData.js
 export const seoPages = [
-  { slug: "coworking-space", title: "Coworking Space" },
+  { slug: "coworking-space-2", title: "Coworking Space" },
   { slug: "private-office", title: "Private Office" },
   { slug: "meeting-room", title: "Meeting Room" },
 ];
