9d367ba4cd6c658470d4d4759a10bd3d241e26be
webtechneeq-developer <developer@webtechneeq.com>
2025-09-12 11:37:51 +0530

    Add files via upload

---
+++ app/(other-pages)/coworking-conference-room/page.jsx
@@ -0,0 +1,83 @@
+<<<<<<< HEAD
+import Agents from "@/components/common/Agents";
+import Brands from "@/components/common/Brands";
+import Testimonials2 from "@/components/common/Testimonials2";
+import Footer1 from "@/components/footer/Footer1";
+import Header1 from "@/components/headers/Header1";
+
+import Banner from "@/components/common/Banner";
+import About from "@/components/otherPages/About";
+import Slider1 from "@/components/property-details/Slider1";
+import React from "react";
+import { allProperties } from "@/data/demoProporties";
+import SeoLinks from "@/components/common/SeoLinks";
+import PageTitleAbout from "@/components/otherPages/PageTitleAbout";
+import BenefitsAbout from "@/components/otherPages/BenefitsAbout";
+
+import CoworkingConferenceRoomTitle from "@/components/otherPages/coworking_pages/CoworkingConferenceRoomTitle";
+import Coworking_Conference_Room from "@/components/otherPages/coworking_pages/Coworking_Conference_Room";
+
+export const metadata = {
+  title: "CoWorking Spaces || Homelengo - Real Estate React Nextjs Template",
+  description: "Homelengo - Real Estate React Nextjs Template",
+};
+export default function page() {
+  return (
+    <>
+      <Header1 />
+      {/* <About /> */}
+      <CoworkingConferenceRoomTitle />
+
+      {/* <Agents /> */}
+      {/* <Brands /> */}
+      <Coworking_Conference_Room />
+      {/* <BenefitsAbout /> */}
+      {/* <Testimonials2 /> */}
+      <Banner />
+      <SeoLinks />
+      <Footer1 />
+    </>
+  );
+}
+=======
+import Agents from "@/components/common/Agents";
+import Brands from "@/components/common/Brands";
+import Testimonials2 from "@/components/common/Testimonials2";
+import Footer1 from "@/components/footer/Footer1";
+import Header1 from "@/components/headers/Header1";
+
+import Banner from "@/components/common/Banner";
+import About from "@/components/otherPages/About";
+import Slider1 from "@/components/property-details/Slider1";
+import React from "react";
+import { allProperties } from "@/data/demoProporties";
+import SeoLinks from "@/components/common/SeoLinks";
+import PageTitleAbout from "@/components/otherPages/PageTitleAbout";
+import BenefitsAbout from "@/components/otherPages/BenefitsAbout";
+
+import CoworkingConferenceRoomTitle from "@/components/otherPages/coworking_pages/CoworkingConferenceRoomTitle";
+import Coworking_Conference_Room from "@/components/otherPages/coworking_pages/Coworking_Conference_Room";
+
+export const metadata = {
+  title: "CoWorking Spaces || Homelengo - Real Estate React Nextjs Template",
+  description: "Homelengo - Real Estate React Nextjs Template",
+};
+export default function page() {
+  return (
+    <>
+      <Header1 />
+      {/* <About /> */}
+      <CoworkingConferenceRoomTitle />
+
+      {/* <Agents /> */}
+      {/* <Brands /> */}
+      <Coworking_Conference_Room />
+      {/* <BenefitsAbout /> */}
+      {/* <Testimonials2 /> */}
+      <Banner />
+      <SeoLinks />
+      <Footer1 />
+    </>
+  );
+}
+>>>>>>> 61c4001d20a11d8cbf78652aa605eb9f34acc89c
---
+++ app/(other-pages)/coworking-space/page.jsx
@@ -0,0 +1,79 @@
+<<<<<<< HEAD
+import Agents from "@/components/common/Agents";
+import Brands from "@/components/common/Brands";
+import Testimonials2 from "@/components/common/Testimonials2";
+import Footer1 from "@/components/footer/Footer1";
+import Header1 from "@/components/headers/Header1";
+
+import Banner from "@/components/common/Banner";
+import About from "@/components/otherPages/About";
+import Slider1 from "@/components/property-details/Slider1";
+import React from "react";
+import { allProperties } from "@/data/demoProporties";
+import SeoLinks from "@/components/common/SeoLinks";
+import PageTitleAbout from "@/components/otherPages/PageTitleAbout";
+import BenefitsAbout from "@/components/otherPages/BenefitsAbout";
+import Coworking_Space from "@/components/otherPages/coworking_pages/Coworking_Space";
+import CoworkingSpacesTitle from "@/components/otherPages/coworking_pages/CoworkingSpacesTitle";
+
+export const metadata = {
+  title: "CoWorking Spaces || Homelengo - Real Estate React Nextjs Template",
+  description: "Homelengo - Real Estate React Nextjs Template",
+};
+export default function page() {
+  return (
+    <>
+      <Header1 />
+      {/* <About /> */}
+      <CoworkingSpacesTitle />
+      {/* <Agents /> */}
+      {/* <Brands /> */}
+      <Coworking_Space />
+      {/* <BenefitsAbout /> */}
+      {/* <Testimonials2 /> */}
+      <Banner />
+      <SeoLinks />
+      <Footer1 />
+    </>
+  );
+}
+=======
+import Agents from "@/components/common/Agents";
+import Brands from "@/components/common/Brands";
+import Testimonials2 from "@/components/common/Testimonials2";
+import Footer1 from "@/components/footer/Footer1";
+import Header1 from "@/components/headers/Header1";
+
+import Banner from "@/components/common/Banner";
+import About from "@/components/otherPages/About";
+import Slider1 from "@/components/property-details/Slider1";
+import React from "react";
+import { allProperties } from "@/data/demoProporties";
+import SeoLinks from "@/components/common/SeoLinks";
+import PageTitleAbout from "@/components/otherPages/PageTitleAbout";
+import BenefitsAbout from "@/components/otherPages/BenefitsAbout";
+import Coworking_Space from "@/components/otherPages/coworking_pages/Coworking_Space";
+import CoworkingSpacesTitle from "@/components/otherPages/coworking_pages/CoworkingSpacesTitle";
+
+export const metadata = {
+  title: "CoWorking Spaces || Homelengo - Real Estate React Nextjs Template",
+  description: "Homelengo - Real Estate React Nextjs Template",
+};
+export default function page() {
+  return (
+    <>
+      <Header1 />
+      {/* <About /> */}
+      <CoworkingSpacesTitle />
+      {/* <Agents /> */}
+      {/* <Brands /> */}
+      <Coworking_Space />
+      {/* <BenefitsAbout /> */}
+      {/* <Testimonials2 /> */}
+      <Banner />
+      <SeoLinks />
+      <Footer1 />
+    </>
+  );
+}
+>>>>>>> 61c4001d20a11d8cbf78652aa605eb9f34acc89c
