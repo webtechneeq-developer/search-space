8102e37665179027584f842981a114c5c6c5f471
webtechneeq-developer <developer@webtechneeq.com>
2025-09-12 11:35:38 +0530

    Add files via upload

---
+++ components/otherPages/coworking_pages/CoworkingFlexiDeskTitle.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Link from "next/link";
+export default function CoworkingFlexiDeskTitle() {
+  return (
+    <section
+      className="flat-title-page"
+      style={{ backgroundImage: "url(/images/page-title/page-title-5.jpg)" }}
+    >
+      <div className="container">
+        <div className="breadcrumb-content">
+          <h1 className="text-center text-white title">Coworking-Flexi-Desk</h1>
+          <ul className="breadcrumb">
+            <li>
+              <Link href={`/`} className="text-white">
+                Home
+              </Link>
+            </li>
+            <li className="text-white">/ Pages</li>
+            <li className="text-white">/ About Us</li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+}
