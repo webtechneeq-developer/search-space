16de7415d0b24ccec9d7b4b2e50970aa2859f628
webtechneeq-developer <developer@webtechneeq.com>
2025-09-12 11:46:05 +0530

    Add files via upload

---
+++ app/(other-pages)/coworking-day-pass/page.jsx
@@ -0,0 +1,39 @@
+import Agents from "@/components/common/Agents";
+import Brands from "@/components/common/Brands";
+import Testimonials2 from "@/components/common/Testimonials2";
+import Footer1 from "@/components/footer/Footer1";
+import Header1 from "@/components/headers/Header1";
+
+import Banner from "@/components/common/Banner";
+import About from "@/components/otherPages/About";
+import Slider1 from "@/components/property-details/Slider1";
+import React from "react";
+import { allProperties } from "@/data/demoProporties";
+import SeoLinks from "@/components/common/SeoLinks";
+import PageTitleAbout from "@/components/otherPages/PageTitleAbout";
+import BenefitsAbout from "@/components/otherPages/BenefitsAbout";
+
+import CoworkingDayPassTitle from "@/components/otherPages/coworking_pages/CoworkingDayPassTitle";
+import Coworking_Day_Pass from "@/components/otherPages/coworking_pages/Coworking_Day_Pass";
+
+export const metadata = {
+  title: "CoWorking Spaces || Homelengo - Real Estate React Nextjs Template",
+  description: "Homelengo - Real Estate React Nextjs Template",
+};
+export default function page() {
+  return (
+    <>
+      <Header1 />
+      {/* <About /> */}
+      <CoworkingDayPassTitle />
+      {/* <Agents /> */}
+      {/* <Brands /> */}
+      <Coworking_Day_Pass />
+      {/* <BenefitsAbout /> */}
+      {/* <Testimonials2 /> */}
+      <Banner />
+      <SeoLinks />
+      <Footer1 />
+    </>
+  );
+}
---
+++ app/(other-pages)/coworking-flexi-desk/page.jsx
@@ -0,0 +1,39 @@
+import Agents from "@/components/common/Agents";
+import Brands from "@/components/common/Brands";
+import Testimonials2 from "@/components/common/Testimonials2";
+import Footer1 from "@/components/footer/Footer1";
+import Header1 from "@/components/headers/Header1";
+
+import Banner from "@/components/common/Banner";
+import About from "@/components/otherPages/About";
+import Slider1 from "@/components/property-details/Slider1";
+import React from "react";
+import { allProperties } from "@/data/demoProporties";
+import SeoLinks from "@/components/common/SeoLinks";
+import PageTitleAbout from "@/components/otherPages/PageTitleAbout";
+import BenefitsAbout from "@/components/otherPages/BenefitsAbout";
+
+import CoworkingFlexiDeskTitle from "@/components/otherPages/coworking_pages/CoworkingFlexiDeskTitle";
+import Coworking_Flexi_Desk from "@/components/otherPages/coworking_pages/Coworking_Flexi_Desk";
+
+export const metadata = {
+  title: "CoWorking Spaces || Homelengo - Real Estate React Nextjs Template",
+  description: "Homelengo - Real Estate React Nextjs Template",
+};
+export default function page() {
+  return (
+    <>
+      <Header1 />
+      {/* <About /> */}
+      <CoworkingFlexiDeskTitle />
+      {/* <Agents /> */}
+      {/* <Brands /> */}
+      <Coworking_Flexi_Desk />
+      {/* <BenefitsAbout /> */}
+      {/* <Testimonials2 /> */}
+      <Banner />
+      <SeoLinks />
+      <Footer1 />
+    </>
+  );
+}
