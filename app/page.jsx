b89f7ae0b1319038078c5d8d8824120cd692c83d
webtechneeq <wt.kraheja2024@gmail.com>
2025-09-15 17:28:47 +0530

    homepage update

--- app/page.jsx
+++ app/page.jsx
@@ -1,17 +1,10 @@
 import Footer1 from "@/components/footer/Footer1";
 import Header1 from "@/components/headers/Header1";
-import Agents from "@/components/common/Agents";
-import Benefit from "@/components/common/Benefit";
 import Blogs from "@/components/homes/home-1/Blogs";
-import Brands from "@/components/common/Brands";
 import Hero from "@/components/homes/home-1/Hero";
 import Locations from "@/components/common/Locations";
-import Properties from "@/components/homes/home-1/Properties";
-import Properties2 from "@/components/homes/home-1/Properties2";
 import Services from "@/components/homes/home-1/Services";
-import Testimonials from "@/components/common/Testimonials";
 import { properties } from "@/data/demoProporties";
-// import AllProperties from "@/components/homes/home-1/AllProperties";
 import PropertyTypes from "@/components/homes/home-1/PropertyTypes";
 
 export const metadata = {
